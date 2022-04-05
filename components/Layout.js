8ab0ba12e4e67a7bd73ca4cc8bc66cafd773325a
MilicTG <milic.tg@gmail.com>
2022-04-05 22:04:02 +0200

    Showcase

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
+import Showcase from "./Showcase";
 import styles from "@/styles/Layout.module.css";
 
 function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -13,6 +17,9 @@ function Layout({ title, keywords, description, children }) {
         <meta name="keywords" content={keywords} />
       </Head>
       <Header />
+
+      {router.pathname === "/" && <Showcase />}
+
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
