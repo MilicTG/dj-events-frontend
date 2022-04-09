f2ae52275e4e96bd8a1d4c53cc52db8fbdf64d01
MilicTG <milic.tg@gmail.com>
2022-04-09 22:33:45 +0200

    Events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-function EventsPage() {
+function EventsPage({ events }) {
   return (
     <Layout>
-      <h1>EventsPage</h1>
+      <h1>Events</h1>
+      {events.lenght === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} event={evt} />
+      ))}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
 }
 
 export default EventsPage;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
+import Link from "next/link";
 
-function HomePage() {
+function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming events</h1>
+      {events.lenght === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} event={evt} />
+      ))}
+
+      {events.lenght > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View all events</a>
+        </Link>
+      )}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events: events.slice(0, 3) },
+  };
 }
 
 export default HomePage;
