a09373309ace155654ca762493a0f2c53a203aff
MilicTG <milic.tg@gmail.com>
2022-04-13 21:43:12 +0200

    problems

--- components/EventItem.js
+++ components/EventItem.js
@@ -8,7 +8,11 @@ function EventItem({ event }) {
     <div className={styles.event}>
       <div className={styles.img}>
         <Image
-          src={event.image ? event.image : "/images/event-default.png"}
+          src={
+            event.image
+              ? event.image.formats.thumbnail.url
+              : "/images/event-default.png"
+          }
           width={170}
           height={100}
           alt=""
